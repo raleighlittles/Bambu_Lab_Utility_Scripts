78e3f1551a8cd5ee6aef606bdf5441603a805f7e
Raleigh Littles III <raleighlittles@gmail.com>
2024-02-27 21:12:11 -0800

    updating list of known dumps

--- RFID_dump_decoder/parse_all_dumps.sh
+++ RFID_dump_decoder/parse_all_dumps.sh
@@ -13,6 +13,6 @@ echo -e "Found $num_dump_files_found dump files! \n"
 find . -name "$json_ext" | while read json_file; do
     echo "Analyzing $json_file.."
     echo -e "\n"
-    python3 py/decode_rfid_tag_dump.py --input-json-dump-file "$json_file"
+    python3 decode_rfid_tag_dump.py --input-json-dump-file "$json_file"
     echo -e "\n\n"
 done
