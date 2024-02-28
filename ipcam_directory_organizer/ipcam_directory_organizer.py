d40c31c154da7252a9fb9e2d73f44eaeda6f44df
Raleigh Littles III <raleighlittles@gmail.com>
2024-02-28 01:04:38 -0800

    formatting

--- ipcam_directory_organizer/ipcam_directory_organizer.py
+++ ipcam_directory_organizer/ipcam_directory_organizer.py
@@ -1,8 +1,6 @@
 import argparse
 import os
-import pdb
 import subprocess
-import sys
 
 # locals
 import ffmpeg_helper
@@ -13,35 +11,29 @@ def combine_videos_from_sequences(directory, video_sequences):
 
     print(f"[DEBUG] Creating '{len(video_sequences)}' videos from sequences")
 
-
     for video_sequence_idx, video_sequence in enumerate(video_sequences):
 
         concat_demuxer_filename = f"{video_sequence_idx}_ffmpeg_demuxer.txt"
 
-        ffmpeg_helper.build_concat_demuxer_list([os.path.join(directory, video_filename) for video_filename in video_sequence], concat_demuxer_filename)
+        ffmpeg_helper.build_concat_demuxer_list([os.path.join(
+            directory, video_filename) for video_filename in video_sequence], concat_demuxer_filename)
 
         ffmpeg_concat_videos_cmd = f"""ffmpeg -f concat -safe 0 -i {concat_demuxer_filename} -c copy '{video_filename_helper.get_basename(video_sequence[0])}.mp4' """
 
-        subprocess.run(ffmpeg_concat_videos_cmd, shell=True, stdout=subprocess.PIPE)
+        subprocess.run(ffmpeg_concat_videos_cmd,
+                       shell=True, stdout=subprocess.PIPE)
 
         # cleanup
         os.remove(concat_demuxer_filename)
 
 
-
-
-        
-
-
-
-    
-
-
 def group_ipcam_videos(directory):
 
-    all_videos = sorted([filename for filename in os.listdir(directory) if filename.endswith(".mp4")])
+    all_videos = sorted([filename for filename in os.listdir(
+        directory) if filename.endswith(".mp4")])
 
-    print(f"[DEBUG] '{len(all_videos)}' videos found in directory '{directory}'")
+    print(
+        f"[DEBUG] '{len(all_videos)}' videos found in directory '{directory}'")
 
     video_sequences = list()
 
@@ -53,11 +45,12 @@ def group_ipcam_videos(directory):
 
         # As long as the index keeps going up, keep adding it to the current sequence
         # once the index goes back to zero (OR a lower number), then stop, and start a new sequence
-  
+
         current_idx = video_filename_helper.get_index(video_filename)
 
-        print(f"[DEBUG] Analyzing element with index: '{current_idx}' and previous index '{previous_idx}'")
-        
+        # print(
+        #    f"[DEBUG] Analyzing element with index: '{current_idx}' and previous index '{previous_idx}'")
+
         # You're at the first element
         if initial_conditions:
             current_video_sequence.append(video_filename)
@@ -70,32 +63,24 @@ def group_ipcam_videos(directory):
                 current_video_sequence.append(video_filename)
                 previous_idx = current_idx
 
-            else: # either the count reset or an element was skipped, in either case, stop incrementing the chain
+            else:  # either the count reset or an element was skipped, in either case, stop incrementing the chain
                 video_sequences.append(current_video_sequence)
-                print(f"[DEBUG] Finished sequence of length '{len(current_video_sequence)}'")
+                print(
+                    f"[DEBUG] Finished sequence of length '{len(current_video_sequence)}'")
                 current_video_sequence = list()
                 previous_idx = current_idx
 
         # end for loop
 
-    return video_sequences
-
-            
-        
-
-
-
-
-
-
-
+    return [sequence for sequence in video_sequences if len(sequence) > 1]
 
 
 if __name__ == "__main__":
 
     argparse_parser = argparse.ArgumentParser()
 
-    argparse_parser.add_argument( "-i", "--input-directory", help="The directory to organize videos in", type=str, required=True)
+    argparse_parser.add_argument(
+        "-i", "--input-directory", help="The directory to organize videos in", type=str, required=True)
 
     argparse_args = argparse_parser.parse_args()
 
--- ipcam_directory_organizer/video_filename_helper.py
+++ ipcam_directory_organizer/video_filename_helper.py
@@ -19,10 +19,12 @@ def parse_video_filename(video_filename):
     A video filename looks like: "ipcam-record.2024-02-25_21-35-33.482.mp4"
     """
 
-    common_prefix, timestamp, current_idx, mp4_extension = video_filename.split(".")
+    common_prefix, timestamp, current_idx, mp4_extension = video_filename.split(
+        ".")
 
     # sanity check
     if common_prefix != "ipcam-record" or mp4_extension != "mp4":
-        raise ValueError(f"Filename '{video_filename}' does not appear to be formatted correctly")
+        raise ValueError(
+            f"Filename '{video_filename}' does not appear to be formatted correctly")
 
     return {"prefix": common_prefix, "timestamp": timestamp, "index": current_idx}
