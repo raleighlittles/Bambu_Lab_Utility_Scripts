59c0e7996e6c269e1a30ded4d535b0bdeacd7678
Raleigh Littles <raleighlittles@gmail.com>
2024-08-03 20:21:07 -0700

    make dump script work when passed a directory

--- RFID_dump_decoder/parse_all_dumps.sh
+++ RFID_dump_decoder/parse_all_dumps.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 
+input_dir=$1
 
 json_ext="*.json"
 
-num_dump_files_found=$(find . -iname "$json_ext" | wc -l)
+num_dump_files_found=$(find "$input_dir" -iname "$json_ext" | wc -l)
 
 echo -e "$(date +'%Y-%m-%d')"
 
 echo -e "Found $num_dump_files_found dump files! \n"
 
-find . -name "$json_ext" | while read json_file; do
+find "$input_dir" -iname "$json_ext" | while read json_file; do
     echo "Analyzing $json_file.."
     echo -e "\n"
     python3 decode_rfid_tag_dump.py --input-json-dump-file "$json_file"
